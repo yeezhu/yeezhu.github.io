6129ee0f8e3413eff6b5f52e252c312b4dd94cd2
yeezhu <julyhaha11@gmail.com>
2017-08-16 11:43:49 +0800

    primary functions

--- api/test.js
+++ api/test.js
@@ -2,7 +2,7 @@
      "code": 200,
      "version": "1.0.12",
      "data": {
-         "token": "呵呵=",
-         "message": "地方的多发发地方的"
+         "token": "test",
+         "message": "testtest"
      }
  }
